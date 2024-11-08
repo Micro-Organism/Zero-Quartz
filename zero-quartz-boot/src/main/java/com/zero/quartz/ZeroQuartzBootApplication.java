a5e6081a250601e06500eabc3e908af03aa795c1
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-08 09:51:19 +0800

    feat: springboot 集成 quartz

---
+++ zero-quartz-boot/src/main/java/com/zero/quartz/ZeroQuartzBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroQuartzBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroQuartzBootApplication.class, args);
+    }
+
+}
